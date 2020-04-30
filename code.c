5e64af129f97f80007b844c824cc26788a221aa2
judypan <32151648+judypan@users.noreply.github.com>
2020-05-01 00:06:01 +0800

    Create code.c

---
+++ code.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+*******************************************************************************/
+
+// Your C code here
+# include<stdio.h>
+
+int main()
+{
+  printf("Hello World!\n");
+  printf("I had a longer code but github has no place to input it");
+  return 0;
+}
